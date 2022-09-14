525b1ce144835a4b42b0acf757323fb5a2f5d4db
Yuto Moriyasu <y.moriyasu@icloud.com>
2022-09-14 16:18:47 +0900

    postgresql setup

--- api/main.go
+++ api/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -22,6 +25,17 @@ func main() {
 	e.GET("/ping", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
 	})
+
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
+		"root",
+		"password",
+		"test",
+		"db",
+		"5432",
+	)
+	db, _ := gorm.Open(postgres.Open(dsn))
+
+	fmt.Println(db.Statement.Vars...)
 
 	httpPort := os.Getenv("HTTP_PORT")
 	if httpPort == "" {
