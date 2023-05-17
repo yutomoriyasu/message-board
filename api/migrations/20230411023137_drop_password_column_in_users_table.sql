88c6d6873cfb83ab3cc98ee57844243c32024412
Yuto Moriyasu <y.moriyasu@icloud.com>
2023-05-17 21:30:46 +0900

    imple db migration

--- api/migrations/20230411023137_drop_password_column_in_users_table.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE users DROP COLUMN password;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE users ADD COLUMN password VARCHAR(255) NOT NULL;
--- +goose StatementEnd
--- api/migrations/20230411135917_create_user_table.sql
+++ api/migrations/20230411135917_create_user_table.sql
@@ -4,9 +4,9 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+  updated_at TIMESTAMP,
+  deleted_at TIMESTAMP
 );
 -- +goose StatementEnd
 
